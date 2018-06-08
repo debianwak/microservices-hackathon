8cdcc7c2ffbca4545b8f731a95191bd59c577be6
tapia.juan@gmail.com <tapia.juan@gmail.com>
2018-06-08 15:52:49 -0500

    added json response for url

--- src/main/java/com/lendingclub/hackathon/linvestors/linvestors/UrlController.java
+++ src/main/java/com/lendingclub/hackathon/linvestors/linvestors/UrlController.java
@@ -20,50 +20,50 @@ public class UrlController {
         log.log(Level.INFO, "json text-->{0}",json);        
        //return "{ fulfillmentText: '[would you like me to send the information to your email?] (https://graph-bot-graphservice.7e14.starter-us-west-2.openshiftapps.com/new-investor-graph)' }";
        return "{\r\n" + 
-       		"  \"conversationToken\": \"\",\r\n" + 
-       		"  \"expectUserResponse\": true,\r\n" + 
-       		"  \"expectedInputs\": [\r\n" + 
-       		"    {\r\n" + 
-       		"      \"inputPrompt\": {\r\n" + 
-       		"        \"richInitialPrompt\": {\r\n" + 
-       		"          \"items\": [\r\n" + 
-       		"            {\r\n" + 
-       		"              \"simpleResponse\": {\r\n" + 
-       		"                \"textToSpeech\": \"Your performance could be\"\r\n" + 
-       		"              }\r\n" + 
-       		"            },\r\n" + 
-       		"            {\r\n" + 
-       		"              \"basicCard\": {\r\n" + 
-       		"                \"title\": \"Your Investment\",\r\n" + 
-       		"                \"formattedText\": \"This is an example of how to investment grow up\",\r\n" + 
-       		"                \"image\": {\r\n" + 
-       		"                  \"url\": \"https://www.africabusinessclassroom.com/wp-content/uploads/2015/07/investment-1.jpg\",\r\n" + 
-       		"                  \"accessibilityText\": \"Image alternate text\"\r\n" + 
-       		"                },\r\n" + 
-       		"                \"buttons\": [\r\n" + 
+       		"   \"conversationToken\":\"\",\r\n" + 
+       		"   \"expectUserResponse\":true,\r\n" + 
+       		"   \"expectedInputs\":[\r\n" + 
+       		"      {\r\n" + 
+       		"         \"inputPrompt\":{\r\n" + 
+       		"            \"richInitialPrompt\":{\r\n" + 
+       		"               \"items\":[\r\n" + 
        		"                  {\r\n" + 
-       		"                    \"title\": \"Read more\",\r\n" + 
-       		"                    \"openUrlAction\": {\r\n" + 
-       		"                      \"url\": \"https://graph-bot-graphservice.7e14.starter-us-west-2.openshiftapps.com/new-investor-graph\"\r\n" + 
-       		"                    }\r\n" + 
+       		"                     \"simpleResponse\":{\r\n" + 
+       		"                        \"textToSpeech\":\"Investment Graphic!\"\r\n" + 
+       		"                     }\r\n" + 
+       		"                  },\r\n" + 
+       		"                  {\r\n" + 
+       		"                     \"basicCard\":{\r\n" + 
+       		"                        \"title\":\"Your Monthly Performance\",\r\n" + 
+       		"                        \"formattedText\":\"This is an example\",\r\n" + 
+       		"                        \"image\":{\r\n" + 
+       		"                           \"url\":\"https://www.africabusinessclassroom.com/wp-content/uploads/2015/07/investment-1.jpg\",\r\n" + 
+       		"                           \"accessibilityText\":\"Image alternate text\"\r\n" + 
+       		"                        },\r\n" + 
+       		"                        \"buttons\":[\r\n" + 
+       		"                           {\r\n" + 
+       		"                              \"title\":\"Read more\",\r\n" + 
+       		"                              \"openUrlAction\":{\r\n" + 
+       		"                                 \"url\":\"https://graph-bot-graphservice.7e14.starter-us-west-2.openshiftapps.com/new-investor-graph\"\r\n" + 
+       		"                              }\r\n" + 
+       		"                           }\r\n" + 
+       		"                        ],\r\n" + 
+       		"                        \"imageDisplayOptions\":\"CROPPED\"\r\n" + 
+       		"                     }\r\n" + 
        		"                  }\r\n" + 
-       		"                ],\r\n" + 
-       		"                \"imageDisplayOptions\": \"CROPPED\"\r\n" + 
-       		"              }\r\n" + 
+       		"               ],\r\n" + 
+       		"               \"suggestions\":[\r\n" + 
+       		"\r\n" + 
+       		"               ]\r\n" + 
        		"            }\r\n" + 
-       		"          ],\r\n" + 
-       		"          \"suggestions\": [\r\n" + 
-       		"            \r\n" + 
-       		"          ]\r\n" + 
-       		"        }\r\n" + 
-       		"      },\r\n" + 
-       		"      \"possibleIntents\": [\r\n" + 
-       		"        {\r\n" + 
-       		"          \"intent\": \"actions.intent.TEXT\"\r\n" + 
-       		"        }\r\n" + 
-       		"      ]\r\n" + 
-       		"    }\r\n" + 
-       		"  ]\r\n" + 
+       		"         },\r\n" + 
+       		"         \"possibleIntents\":[\r\n" + 
+       		"            {\r\n" + 
+       		"               \"intent\":\"actions.intent.TEXT\"\r\n" + 
+       		"            }\r\n" + 
+       		"         ]\r\n" + 
+       		"      }\r\n" + 
+       		"   ]\r\n" + 
        		"}";
     }
 
