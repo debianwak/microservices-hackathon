6c17955dd4156e37b4a1d7266cea4eaf627cb068
Alfonso Valdez <alfonso.valdez@globant.com>
2018-06-18 17:10:47 -0500

    test gson format message

--- src/main/java/com/lendingclub/hackathon/linvestors/linvestors/UrlController.java
+++ src/main/java/com/lendingclub/hackathon/linvestors/linvestors/UrlController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 
 @RestController
@@ -15,10 +17,15 @@ public class UrlController {
     private Logger log = Logger.getLogger("UrlController.class");
 
     @RequestMapping(value = "/url", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
-    @ResponseBody
-    public String getURL(@RequestBody String json) {
-       log.log(Level.INFO, "json text-->{0}",json);        
-       return "\"messages\": [{\"buttons\": [{\"openUrlAction\": {\"url\": \"https://graph-bot-graphservice.7e14.starter-us-west-2.openshiftapps.com/new-investor-graph\"},\"title\": \"Graph Demo\"}],\"formattedText\": \"Investment Graphic!\",\"image\": {"+
-"\"url\": \"https://www.africabusinessclassroom.com/wp-content/uploads/2015/07/investment-1.jpg\",\"accessibilityText\": \"Investment Graphic!\"},\"platform\": \"google\",\"subtitle\": \"LendingClub\",\"title\": \"LendingClub Graph\",\"type\": \"basic_card\"}]";
-    }
+@ResponseBody
+public JsonObject getURL(@RequestBody String json) {
+   log.log(Level.INFO, "json text-->{0}",json);
+
+   String jsonString = "\"messages\": [{\"buttons\": [{\"openUrlAction\": {\"url\": \"https://graph-bot-graphservice.7e14.starter-us-west-2.openshiftapps.com/new-investor-graph\"},\"title\": \"Graph Demo\"}],\"formattedText\": \"Investment Graphic!\",\"image\": {"+
+	   "\"url\": \"https://www.africabusinessclassroom.com/wp-content/uploads/2015/07/investment-1.jpg\",\"accessibilityText\": \"Investment Graphic!\"},\"platform\": \"google\",\"subtitle\": \"LendingClub\",\"title\": \"LendingClub Graph\",\"type\": \"basic_card\"}]";
+	log.log(Level.INFO, "json text response-->{0}",jsonString);
+   JsonParser parser = new JsonParser();
+   JsonObject objectJson = parser.parse(jsonString).getAsJsonObject();
+   return objectJson;
+}
 }
