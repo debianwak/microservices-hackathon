e9abc2ec5c9c5d11be0c5fa362f094234ed80b70
Alfonso Valdez <alfonso.valdez@globant.com>
2018-06-18 15:55:16 -0500

    change to test format

--- src/main/java/com/lendingclub/hackathon/linvestors/linvestors/UrlController.java
+++ src/main/java/com/lendingclub/hackathon/linvestors/linvestors/UrlController.java
@@ -17,8 +17,9 @@ public class UrlController {
     @RequestMapping(value = "/url", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
     @ResponseBody
     public String getURL(@RequestBody String json) {
-       log.log(Level.INFO, "json text-->{0}",json);        
-       return "{\"messages\": [{\"buttons\": [{\"openUrlAction\": {\"url\": \"https://graph-bot-graphservice.7e14.starter-us-west-2.openshiftapps.com/new-investor-graph\"},\"title\": \"Graph Demo\"}],\"formattedText\": \"Investment Graphic!\",\"image\": {"+
-	   "\"url\": \"https://www.africabusinessclassroom.com/wp-content/uploads/2015/07/investment-1.jpg\",\"accessibilityText\": \"Investment Graphic!\"},\"platform\": \"google\",\"subtitle\": \"LendingClub\",\"title\": \"LendingClub Graph\",\"type\": \"basic_card\"}]}";
+       log.log(Level.INFO, "json text-->{0}",json);  
+	   String response = "\"messages\": [{\"imageUrl\": \"https://www.africabusinessclassroom.com/wp-content/uploads/2015/07/investment-1.jpg\",\"platform\": \"google"\","\"type"\": 3}]";
+	   log.log(Level.INFO, "json text response-->{0}",response); 
+       return response;
     }
 }
